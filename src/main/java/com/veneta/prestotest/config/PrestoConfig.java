971b2936ce9c197893dc56c0aa9f7f88df28074e
heavenring <anstn0104@naver.com>
2026-02-04 20:54:34 +0900

    add: presto application.yml & config

---
+++ src/main/java/com/veneta/prestotest/config/PrestoConfig.java
@@ -0,0 +1,15 @@
+package com.veneta.prestotest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+@Configuration
+public class PrestoConfig {
+    @Bean
+    public Scheduler prestoScheduler() {
+        // JDBC의 Blocking 작업을 처리할 전용 스케줄러 생성
+        return Schedulers.newBoundedElastic(10, 100, "presto-pool");
+    }
+}
