593745652928ddeb8ac8b086386410dac747b014
msanstn <msanstn@gmail.com>
2026-02-04 09:43:30 +0900

    first Setting

---
+++ src/main/java/com/veneta/prestotest/PrestoTestApplication.java
@@ -0,0 +1,13 @@
+package com.veneta.prestotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrestoTestApplication.class, args);
+    }
+
+}
